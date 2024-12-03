d8809b6d4ac9c05777fd462478c6d45aebafee38
Michael Lim <83725834+MichaelKrLim@users.noreply.github.com>
2024-12-03 23:54:52 +0000

    day 3

--- 2024/3.cpp
+++ 2024/3.cpp
@@ -1,31 +1,27 @@
 #include <iostream>
+#include <regex>
 #include <fstream>
 #include <string>
 
-int get_num(std::string input, int i)
-{
-    auto index{i-1};
-    while(isdigit(input[++index]))
-    return std::stoi(input.substr(i, index-i));
-}
-
-int resolve_input(std::string input)
-{
-    static long long result{};
-    for(int i{0}; i<input.size()-1; ++i)
-    {
-        if(input.substr(i, 4) == 'mul(')
-        {
-            
-        }
-    }
-    return result;
-}
-
 int main()
 {
+    std::fstream in("3.in");
     std::string input;
-    std::cin >> input;
-    long long result = resolve_input(input);
+    std::string line{};
+    while(std::getline(in, line)) 
+    {
+        input += line;
+    }
+    long long result{0};
+    std::regex regx("mul\\(\\d{1,3},\\d{1,3}\\)");
+    for(std::sregex_iterator rit(input.begin(), input.end(), regx), rend; rit != rend; ++rit) 
+    {
+        std::string operation = rit->str().substr(4, rit->str().size()-5);
+        std::stringstream ss(operation);
+        std::string lhs, rhs;
+        getline(ss, lhs, ',');
+        getline(ss, rhs, ',');
+        result += std::stoi(lhs)*std::stoi(rhs);
+    }
     std::cout << result << "\n";
 }
